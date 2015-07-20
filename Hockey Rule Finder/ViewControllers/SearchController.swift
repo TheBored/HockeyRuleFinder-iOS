35bce1a82a7a0fab467be8fdbfb94fe9f20d5ab3
Brian Maxwell <brianmxwll@gmail.com>
2015-07-19 22:16:22 -0400

    Start of search page
    Just getting the basics loaded into the page - search bar/results
    table. placeholder rules are there for now, to show that the UI is
    loading as intended. UISearchBar is wired properly, but search is not
    fired after user enters text.

--- Hockey Rule Finder/ViewControllers/SearchController.swift
+++ Hockey Rule Finder/ViewControllers/SearchController.swift
@@ -8,7 +8,13 @@
 
 class SearchController: UIViewController {
     
+   
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    var searchActive = false;
+    var searchText = "";
+    var tableData = [Rule]();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,11 +24,49 @@ class SearchController: UIViewController {
             menuButton.action = "revealToggle:"
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
+        
+        tableData = RuleDataServices.GetRulesForSection(1); //Placeholder, to test that the table works.
+    }
+    
+    func search() {
+        return;
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        self.searchText = searchText;
+        return;
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.tableData.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath:indexPath) as! UITableViewCell;
+        cell.textLabel!.text = tableData[indexPath.row].ruleName;
+        return cell;
     }
 }
 
